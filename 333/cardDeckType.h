e87b4408ffb002c47ab77a2f7078d0e3a2c8ad6e
Jeffrey Nino <53096459+jeffreynino@users.noreply.github.com>
2021-03-02 19:23:07 -0800

    Create cardDeckType.h

---
+++ 333/cardDeckType.h
@@ -0,0 +1,22 @@
+enum GAME_TYPE {NORMAL, DUPLICATE, TIE0, TIE3, TIE33, TIE_OTHER,
+                PLAYER1_WIN, PLAYER2_WIN};
+                
+class cardDeckType
+{
+    public:
+    cardDeckType(GAME_TYPE = NORMAL); // constructor
+    int drawCard();
+    int cardsRemaining();
+    void shuffle();
+    void printDeck();
+    void initDeck();
+    void setDeck(GAME_TYPE);
+    
+    private:
+    int deck[52];
+    int nextCard = 0;
+    void duplicate();
+    void tie ( GAME_TYPE );
+    void player( GAME_TYPE);
+    
+};
