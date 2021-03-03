1ce903100c1ce0034076d022d7da92ea8c058bde
Jeffrey Nino <53096459+jeffreynino@users.noreply.github.com>
2021-03-02 18:55:10 -0800

    Create coutpractice.cpp

---
+++ coutpractice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "CS135 Assignment 1" << endl;
+	cout << "Jeffrey Crillin Nino" << endl;
+	cout << " " << endl;
+	cout << " JJJJJJJJJJ   CCCCCCCC  NN       NN" << endl;
+	cout << "     JJ     CC          NNN      NN" << endl;
+	cout << "     JJ     CC          NN N     NN" << endl;
+	cout << "     JJ     CC          NN  N    NN" << endl;
+	cout << "     JJ     CC          NN   N   NN" << endl;
+	cout << "J    JJ     CC          NN    N  NN" << endl;
+	cout << "J    JJ     CC          NN     N NN" << endl;
+	cout << " JJJJJ        CCCCCCCC  NN      NNN" << endl;
+}
