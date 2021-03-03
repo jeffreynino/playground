c3901c926d836fa572cd444400de7627622b992b
Jeffrey Nino <53096459+jeffreynino@users.noreply.github.com>
2021-03-02 19:50:25 -0800

    Update ws14.cpp

--- worksheets/ws14.cpp
+++ worksheets/ws14.cpp
@@ -1,3 +1,8 @@
+// Jeffrey Nino, 5005572268, CS135 Section 1004, Worksheet 13
+// Description: circumference calculator
+// Input: radius
+// Output: circumference
+
 #include <iostream>
 
 using namespace std;
